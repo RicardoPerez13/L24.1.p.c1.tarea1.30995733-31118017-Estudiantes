c7b8dc1b8adf2f1a8ea26bbf95d04f17c2331f57
RicardoPerez13 <1001.30995733.ucla@gmail.com>
2024-07-04 14:01:06 -0400

    (feature): primer commit

---
+++ js/Principal.js
@@ -0,0 +1,14 @@
+/*2. ESTUDIANTES
+
+En la UCLA, el DCYT desea conocer algunas estadísticas de sus estudiantes. 
+De cadaestudiante se conoce: su cédula, sexo y sus tres notas parciales. 
+Se requiere conocer por el DCYT: 
+a) nota total obtenida por el estudiante 
+b) la mayor nota obtenida 
+c) De losestudiantes femeninos cantidad de estudiantes aprobadas, 
+d) promedio de notas
+
+El DCYT suministra la siguiente información demostrativa, para 4 estudiantes: (cedula, sexo,
+notap1, notap2, notap3) (13457856, M, 15, 20, 25) (15432551, F, 10, 10, 10) (32455678, M,
+18, 22, 20) (28678900, F, 18, 25, 24)*/
+
